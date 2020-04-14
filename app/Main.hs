17e7eddfb25fbe419f2984928c7d1b802531a150
Nick Lofaro <nicholas@cabansystems.com>
2020-04-13 21:29:20 -0700

    Parse file path

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 module Main where
 
-import Lib
+import Options.Applicative
+
+type ItemIndex = Int
+
+defaultDataPath :: FilePath
+defaultDataPath = "~/.to-do.yaml"
+
+dataPathParser :: Parser FilePath
+dataPathParser = strOption $
+    value defaultDataPath
+    <> long "data-path"
+    <> short 'p'
+    <> metavar "DATAPATH"
+    <> help ("path to data file (default " ++ defaultDataPath ++ ")")
+
+itemIndexParser :: Parser ItemIndex
+itemIndexParser = argument auto (metavar "ITEMINDEX" <> help "index of item")
 
 main :: IO ()
-main = someFunc
+main = do
+    dataPath <- execParser (info (dataPathParser) (progDesc "To-do list manager"))
+    putStrLn $ "dataPath=" ++ show dataPath
+
+    --itemIndex <- execParser (info (itemIndexParser) (progDesc "To-do list manager"))
+    --putStrLn $ "itemIndex=" ++ show itemIndex
